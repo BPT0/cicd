54dc700175119b17abd3cbd67403e2692e512109
BPT0 <kbs95123@gmail.com>
2024-07-15 11:58:34 +0900

    workflow 정상 테스트

--- src/main/java/dev/mvc/cicd/CICDCont.java
+++ src/main/java/dev/mvc/cicd/CICDCont.java
@@ -6,28 +6,33 @@ import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
+@Slf4j // lombok 의 log 기능을 사용
 @RestController
 public class CICDCont {
+  public CICDCont() {
+    System.out.println("-> CICDCont created.");
+  }
+
   // http://localhost:9091/cicd?sw=-1
   // http://localhost:9091/cicd?sw=0
   // http://localhost:9091/cicd?sw=1
-  // http://localhost:9091/cicd?sw=2
   @GetMapping("/cicd")
-  public String cicd(@RequestParam(name="sw", defaultValue = "1") Integer sw) {
+  public String cicd(@RequestParam(defaultValue = "1") Integer sw) { // defaultValue = "1" -> 1
     String msg = "";
     
+    // log 기능을 이용하여 글자 색깔에 차이를 두어 디버깅이 가능함
     if (sw == -1) {
       msg = "/cide 호출됨, error 로그";
       log.error(msg);
     } else if (sw == 0) {
       msg = "/cide 호출됨, warn 로그";
       log.warn(msg);
-    } else if (sw == 1) {
+    }else if (sw == 1) {
       msg = "/cicd 호출됨, info 로그";
       log.info(msg);
-    } else if (sw == 2) {
-      msg = "/cide workflow 테스트";
-      log.info(msg);
     }
+    return "<h3>" + msg + "</h3>";
+  }
+  
+}
 
